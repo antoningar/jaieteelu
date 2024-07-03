b2616e5b4fbcafecc9f17116541217f69516c926
agar <antoningaranto@protonmail.com>
2024-07-04 00:18:42 +0200

    feat: start twitter bot

---
+++ bot/twitter/OAuthClient.cs
@@ -0,0 +1,38 @@
+using System.Text;
+using OAuth;
+
+namespace twitter;
+
+public class OAuthClient
+{
+    private const string CONSUMER_KEY = "";
+    private const string CONSUMER_SECRET = "";
+    private const string ACCESS_TOKEN = "";
+    private const string ACCESS_TOKEN_SECRET = "";
+
+    public static async Task<string> MakeAuthenticatedRequestAsync(string resourceUrl, HttpMethod method, string body)
+    {
+        OAuthRequest oauth = new()
+        {
+            Method = method.ToString(),
+            Type = OAuthRequestType.ProtectedResource,
+            SignatureMethod = OAuthSignatureMethod.HmacSha1,
+            ConsumerKey = CONSUMER_KEY,
+            ConsumerSecret = CONSUMER_SECRET,
+            Token = ACCESS_TOKEN,
+            TokenSecret = ACCESS_TOKEN_SECRET,
+            RequestUrl = resourceUrl
+        };
+
+        string? authHeader = oauth.GetAuthorizationHeader();
+
+        using HttpClient httpClient = new();
+        httpClient.DefaultRequestHeaders.Add("Authorization", authHeader);
+
+        HttpRequestMessage request = new (method, resourceUrl);
+        request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await httpClient.SendAsync(request);
+        return await response.Content.ReadAsStringAsync();
+    }
+}
---
+++ bot/twitter/Program.cs
@@ -0,0 +1,9 @@
+﻿using twitter;
+
+OAuthClient client = new();
+string url = "https://api.twitter.com/2/tweets";
+string simpleTweet = "{\"text\": \"Tiens tiens tiens\"}";
+string replyTweet = "{\"text\": \"Tiens tiens retiens\", \"reply\": {\"in_reply_to_tweet_id\": \"1808624245003452927\"}}";
+
+string result = await OAuthClient.MakeAuthenticatedRequestAsync(url, HttpMethod.Post, replyTweet);
+Console.WriteLine(result);
