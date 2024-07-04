b2810a127890dd0843cfec8d1b994f8f997f5b26
agar <antoningaranto@protonmail.com>
2024-07-04 23:54:44 +0200

    feat(candidats): select candidats

---
+++ bot/twitter/Candidat.cs
@@ -0,0 +1,6 @@
+namespace twitter;
+
+public record Candidat(string nom, string circonscription, string[] anecdotes, string[] refs)
+{
+    
+}
--- bot/twitter/Program.cs
+++ bot/twitter/Program.cs
@@ -1,9 +1,15 @@
 ﻿using twitter;
 
-OAuthClient client = new();
-string url = "https://api.twitter.com/2/tweets";
-string simpleTweet = "{\"text\": \"Tiens tiens tiens\"}";
-string replyTweet = "{\"text\": \"Tiens tiens retiens\", \"reply\": {\"in_reply_to_tweet_id\": \"1808624245003452927\"}}";
+Candidat candidat = CandidatService.GetCandidats();
+Console.WriteLine(candidat.nom);
+CandidatService.SaveCandidat(candidat.nom);
 
-string result = await OAuthClient.MakeAuthenticatedRequestAsync(url, HttpMethod.Post, replyTweet);
-Console.WriteLine(result);
+async Task SendTweetAsync(IEnumerable<string> tweets){
+    OAuthClient client = new();
+    string url = "https://api.twitter.com/2/tweets";
+    string simpleTweet = "{\"text\": \"Tiens tiens tiens\"}";
+    string replyTweet = "{\"text\": \"Tiens tiens retiens\", \"reply\": {\"in_reply_to_tweet_id\": \"1808624245003452927\"}}";
+
+    string result = await OAuthClient.MakeAuthenticatedRequestAsync(url, HttpMethod.Post, replyTweet);
+    Console.WriteLine(result);
+}
