19e891d83ea54ff691018d44d88713989a7a929e
agar <antoningaranto@protonmail.com>
2024-07-16 22:02:46 +0200

    add liste elected

---
+++ datas/elected/script.py
@@ -0,0 +1,12 @@
+import csv
+
+chemin_fichier = 'liste_deputes_libre_office.csv'
+chemin_sortie = 'elus.txt'
+
+with open(chemin_fichier, newline='', encoding='utf-8') as fichier:
+    lecteur = csv.DictReader(fichier)
+
+    with open(chemin_sortie, 'w', encoding='utf-8') as fichier_sortie:
+        for ligne in lecteur:
+            nom_prenom = f"{ligne['Nom']} {ligne['Prénom']}\n"
+            fichier_sortie.write(nom_prenom)
