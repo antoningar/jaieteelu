12ae74d1b8a00bfc29a66e2c4de825513da384c5
agar <antoningaranto@protonmail.com>
2024-07-23 22:28:16 +0200

    deputy: update deputy spliting

--- bot/twitter/Deputy/Deputy.cs
+++ bot/twitter/Deputy/Deputy.cs
@@ -1,10 +1,17 @@
+using System.Text;
+using System.Text.Json.Serialization;
+
 namespace twitter.Deputy;
 
 public class Deputy(string name, string constituency, string[] facts, string[] refs)
 {
+    [JsonPropertyName("nom")]
     public string Name { get; set; } = name;
+    [JsonPropertyName("circonscription")]
     public string Constituency { get; set; } = constituency;
+    [JsonPropertyName("anecdotes")]
     public string[] Facts { get; set; } = facts;
+    [JsonPropertyName("refs")]
     public string[] Refs { get; set; } = refs;
 
     public List<string> SplitDeputy()
@@ -15,8 +22,39 @@ public class Deputy(string name, string constituency, string[] facts, string[] r
         ];
         
         return parts
-            .Concat(Facts)
+            .Concat(SplitFacts())
             .Concat(Refs)
             .ToList();
+    }
+
+    private string[] SplitFacts()
+    {
+        const int MAX_SIZE = 280;
+        string[] result = [];
+
+        foreach (string fact in Facts)
+        {
+            string[] substrings = [];
+            
+            string[] words = fact.Split();
+            StringBuilder currentString = new();
+            
+            foreach (string word in words)
+            {
+                if (currentString.Length + word.Length + 1 > MAX_SIZE)
+                {
+                    substrings = substrings.Append(currentString.ToString().Trim()).ToArray();
+                    currentString.Clear();
+                }
+                currentString.Append(word + " ");
+            }
+            if (currentString.Length > 0)
+            {
+                substrings = substrings.Append(currentString.ToString().Trim()).ToArray();
+            }
+            result = result.Concat(substrings).ToArray();
+        }
+        
+        return result;
     }
 }
