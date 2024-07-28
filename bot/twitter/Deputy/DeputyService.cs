d6246ab128b3793627953e9b7f55b71fc2ebcc7d
agar <antoningaranto@protonmail.com>
2024-07-28 22:13:38 +0200

    cron: adapt paths

--- bot/twitter/Deputy/DeputyService.cs
+++ bot/twitter/Deputy/DeputyService.cs
@@ -1,39 +1,45 @@
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace twitter.Deputy;
 
 public static class DeputyService
 {
-    private const string BASE_DEPUTY_FILENAME = "deputies.json";
-    private const string OLD_DEPUTY_FILENAME = "old_deputies.json";
 
-    public static Deputy GetDeputies()
+    public static Deputy GetDeputies(string baseDeputyFilename, string oldDeputyFilename)
     {
-        IEnumerable<Deputy> baseDeputies = GetBaseDeputies();
-        List<string> oldDeputies = GetOldDeputies().ToList();
+        IEnumerable<Deputy> baseDeputies = GetBaseDeputies(baseDeputyFilename);
+        List<string> oldDeputies = GetOldDeputies(oldDeputyFilename).ToList();
 
         Deputy deputy = GetDeputy(baseDeputies.ToArray(), oldDeputies);
 
         return deputy;
     }
 
-    public static void SaveDeputy(string deputyName)
+    public static void SaveDeputy(string deputyName, string filename)
     {
-        List<string> oldDeputies = GetOldDeputies().ToList();
+        List<string> oldDeputies = GetOldDeputies(filename).ToList();
         oldDeputies.Add(deputyName);
-        string jsonString = JsonSerializer.Serialize(oldDeputies);
-        File.WriteAllText(OLD_DEPUTY_FILENAME, jsonString);
+        
+        JsonSerializerOptions jsonOptions = new()
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+        string jsonString = JsonSerializer.Serialize(oldDeputies, jsonOptions);
+        
+        File.WriteAllText(filename, jsonString, Encoding.UTF8);
     }
 
-    private static IEnumerable<Deputy> GetBaseDeputies()
+    private static IEnumerable<Deputy> GetBaseDeputies(string filename)
     {
-        string content = File.ReadAllText(BASE_DEPUTY_FILENAME);
+        string content = File.ReadAllText(filename);
         return JsonSerializer.Deserialize<IEnumerable<Deputy>>(content)!;
     }
 
-    private static IEnumerable<string> GetOldDeputies()
+    private static IEnumerable<string> GetOldDeputies(string filename)
     {
-        string content = File.ReadAllText(OLD_DEPUTY_FILENAME);
+        string content = File.ReadAllText(filename);
         return JsonSerializer.Deserialize<IEnumerable<string>>(content)!;
     }
 
---
+++ bot/twitter/Deputy/FileOptions.cs
@@ -0,0 +1,8 @@
+namespace twitter.Deputy;
+
+public class FileOptions
+{
+    public string BaseDeputyFilePath { get; set; } = string.Empty;
+    public string OldDeputyFilePath { get; set; } = string.Empty;
+    public string PictureFilePath { get; set; } = string.Empty;
+}
